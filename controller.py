4b990c8b0fc7429b1d148a42ee2d642e423a90ec
WU,BING-YING <kos8123@gmail.com>
2024-09-06 02:45:58 +0800

    new feature

--- controller.py
+++ controller.py
@@ -1,8 +1,20 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from service import UserService
 from model import SessionLocal
 from pydantic import BaseModel
+import os
+from linebot.v3 import WebhookHandler
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    ShowLoadingAnimationRequest
+)
+import google.generativeai as genai
+import json
 router = APIRouter()
 
 class UserCreate(BaseModel):
@@ -10,12 +22,34 @@ class UserCreate(BaseModel):
     height: float
     weight: float
 
+class Test(BaseModel):
+    key: str
+  
+
 def get_db():
     db = SessionLocal()  
     try:
         yield db        #https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/#sub-dependencies-with-yield
     finally:
         db.close()
+
+handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
+configuration = Configuration(access_token=os.getenv("ACCESS_TOKEN"))
+genai.configure(api_key=os.getenv("API_KEY"))
+
+
+
+@router.get("/api/test")
+def HelloWorld():
+    return "Hello World"
+
+@router.post("/api/test")
+def test(cxcxc: Test):
+    if cxcxc.key == "cxcxc":
+        return {"message": "Succeeded"}
+    else:
+        return {"message": "Failed"}
+    
 
 @router.get("/api/user")
 def get_allUsers(db: Session = Depends(get_db)):
@@ -27,9 +61,51 @@ def get_allUsers(db: Session = Depends(get_db)):
 def add_user(user: UserCreate, db: Session = Depends(get_db)):
     service = UserService(db)
     try:
-        service.add_user(user.name, user.height, user.height)
+        service.add_user(user.name, user.height, user.weight)
         return {"status": "successed"}
     except Exception as e:
         return {"status": "error"}
 
+@router.post("/api/llm")
+async def linebot(request: Request, db: Session = Depends(get_db)):
+    body = await request.body()
+    body = body.decode()
+    json_data = json.loads(body)
 
+    try:
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        event = json_data['events'][0]
+        reply_token = event['replyToken']
+        user_id = event['source']['userId']
+        msg_type = event['message']['type']
+            
+        if msg_type == "text":
+            msg = event['message']['text']
+            with ApiClient(configuration) as api_client:
+                line_bot_api = MessagingApi(api_client)
+                line_bot_api.show_loading_animation(ShowLoadingAnimationRequest(
+                chatId=user_id, loadingSeconds=20))
+
+                model = genai.GenerativeModel('gemini-1.5-flash')
+                response = model.generate_content(f"Only parse the three attributes of name(str)/weight(float)/height(float) in the sentence, and return Json format after parsing, No symbols required, just json.{msg}")
+                reply_msg = response.text
+                line_bot_api.reply_message(ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text="新增使用者成功"),]))
+                service = UserService(db)
+                reply_msg = json.loads(reply_msg)
+                service.add_user(reply_msg["name"], float(reply_msg["height"]), float(reply_msg["weight"]))
+        
+        else:
+                with ApiClient(configuration) as api_client:
+                    line_bot_api = MessagingApi(api_client)
+                    line_bot_api.reply_message(
+                        ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text='你傳的不是文字訊息'),]))
+
+    except Exception as e:
+        detail = e.args[0]
+        print(detail)
+
+
+
+
+    
--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
 FROM python:3
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
-CMD ["python3", "FastAPI.py"]
+EXPOSE 3306
+CMD ["python3", "main.py"]
