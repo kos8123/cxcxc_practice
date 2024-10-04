b2f5ed8057f4fb0560668ca168b219bada9f0921
WU,BING-YING <76834811+kos8123@users.noreply.github.com>
2024-10-04 16:58:03 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -67,44 +67,44 @@ def add_user(user: UserCreate, db: Session = Depends(get_db)):
     except Exception as e:
         return {"status": "error"}
 
-@router.post("/api/llm")
-async def linebot(request: Request, db: Session = Depends(get_db)):
-    body = await request.body()
-    body = body.decode()
-    json_data = json.loads(body)
+# @router.post("/api/llm")
+# async def linebot(request: Request, db: Session = Depends(get_db)):
+#     body = await request.body()
+#     body = body.decode()
+#     json_data = json.loads(body)
 
-    try:
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        event = json_data['events'][0]
-        reply_token = event['replyToken']
-        user_id = event['source']['userId']
-        msg_type = event['message']['type']
+#     try:
+#         signature = request.headers['X-Line-Signature']
+#         handler.handle(body, signature)
+#         event = json_data['events'][0]
+#         reply_token = event['replyToken']
+#         user_id = event['source']['userId']
+#         msg_type = event['message']['type']
             
-        if msg_type == "text":
-            msg = event['message']['text']
-            with ApiClient(configuration) as api_client:
-                line_bot_api = MessagingApi(api_client)
-                line_bot_api.show_loading_animation(ShowLoadingAnimationRequest(
-                chatId=user_id, loadingSeconds=20))
+#         if msg_type == "text":
+#             msg = event['message']['text']
+#             with ApiClient(configuration) as api_client:
+#                 line_bot_api = MessagingApi(api_client)
+#                 line_bot_api.show_loading_animation(ShowLoadingAnimationRequest(
+#                 chatId=user_id, loadingSeconds=20))
 
-                model = genai.GenerativeModel('gemini-1.5-flash')
-                response = model.generate_content(f"Only parse the three attributes of name(str)/weight(float)/height(float) in the sentence, and return Json format after parsing, No symbols required, just json.{msg}")
-                reply_msg = response.text
-                line_bot_api.reply_message(ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text="新增使用者成功"),]))
-                service = UserService(db)
-                reply_msg = json.loads(reply_msg)
-                service.add_user(reply_msg["name"], float(reply_msg["height"]), float(reply_msg["weight"]))
+#                 model = genai.GenerativeModel('gemini-1.5-flash')
+#                 response = model.generate_content(f"Only parse the three attributes of name(str)/weight(float)/height(float) in the sentence, and return Json format after parsing, No symbols required, just json.{msg}")
+#                 reply_msg = response.text
+#                 line_bot_api.reply_message(ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text="新增使用者成功"),]))
+#                 service = UserService(db)
+#                 reply_msg = json.loads(reply_msg)
+#                 service.add_user(reply_msg["name"], float(reply_msg["height"]), float(reply_msg["weight"]))
         
-        else:
-                with ApiClient(configuration) as api_client:
-                    line_bot_api = MessagingApi(api_client)
-                    line_bot_api.reply_message(
-                        ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text='你傳的不是文字訊息'),]))
+#         else:
+#                 with ApiClient(configuration) as api_client:
+#                     line_bot_api = MessagingApi(api_client)
+#                     line_bot_api.reply_message(
+#                         ReplyMessageRequest(reply_token=reply_token,messages=[TextMessage(text='你傳的不是文字訊息'),]))
 
-    except Exception as e:
-        detail = e.args[0]
-        print(detail)
+#     except Exception as e:
+#         detail = e.args[0]
+#         print(detail)
 
 
 
